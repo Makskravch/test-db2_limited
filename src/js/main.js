0ba300e7980e16632fd768635c4aacf09ef9d05b
Maksym Kravchuk <kravchukmaxym@gmail.com>
2020-07-25 12:24:16 +0300

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+$(function () {
+
+	$('a[href*="#"]').on('click', function (event) {
+
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+
+			event.preventDefault();
+
+			let hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top - 100
+			}, 500, function () {
+				window.location.hash = hash;
+			});
+		}
+	});
+})
